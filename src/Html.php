f23bd3d873b3977838dc6428bfcee40d8f2f8c5d
Igor Tarasov <develop@dicr.org>
2019-05-09 23:38:24 +0500

    Html::meta

--- src/Html.php
+++ src/Html.php
@@ -9,16 +9,28 @@ namespace dicr\helper;
  */
 class Html extends \yii\helpers\Html
 {
-
     /**
      * @inheritdoc
      */
     public static function esc($str)
     {
+
+
         if ($str === null || $str === '') {
             return '';
         }
 
         return parent::encode((string) $str);
+    }
+
+    /**
+     * Html meta tag
+     *
+     * @param array $options
+     * @return string
+     */
+    public static function meta(array $options)
+    {
+        return self::tag('meta', $options);
     }
 }
