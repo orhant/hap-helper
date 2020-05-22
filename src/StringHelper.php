7ef2a908900e3a770df0d43d6cff6728b33df359
Igor A Tarasov <develop@dicr.org>
2020-05-22 05:08:01 +0500

    StringHelper::replaceVars

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -3,12 +3,15 @@
  * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 15.01.20 12:39:10
+ * @version 22.05.20 05:07:45
  */
 
 declare(strict_types = 1);
 
 namespace dicr\helper;
+
+use function preg_match;
+use function preg_replace_callback;
 
 /**
  * String Helper.
@@ -29,5 +32,57 @@ class StringHelper extends \yii\helpers\StringHelper
         $rest = mb_substr($string, 1, null, $encoding);
 
         return mb_strtolower($firstChar, $encoding) . $rest;
+    }
+
+    /**
+     * Подмена переменных в строке.
+     *
+     * Подменяет строки шаблонных переменных вида ${vars|attribute|attribute}.
+     * Значения берутся из массива vars. Пример:
+     *
+     * ```php
+     * Html::replaceVars("Купить ${prod|name}", [
+     *   'prod' => ['name' => "Автомобиль"]
+     * ]);
+     * ```
+     *
+     * @param string|null $string строка с переменными
+     * @param array $vars значения для подмены (многомерный массив обхектов)
+     * @param array $opts опции
+     * - bool $cleanVars - удалять ненайденные переменные
+     * - bool $cleanText - удалять весь текст, если есть ненайденные переменные
+     * @return string
+     */
+    public static function replaceVars($string, array $vars = [], array $opts = [])
+    {
+        // пропускаем пустые строки
+        $string = (string)$string;
+        if ($string === '') {
+            return '';
+        }
+
+        // регулярное выражение
+        $regex = '~\$\{([^\}]+)\}~um';
+
+        // находим и заменяем все переменные
+        $string = preg_replace_callback($regex, static function(array $matches) use ($vars) {
+            // получаем ключ
+            $key = explode('|', $matches[1]);
+
+            // комплексное название переменной
+            return ArrayHelper::getValue($vars, $key) ?? $matches[0];
+        }, $string);
+
+        // удаляем весь текст если переменные не есть ненайденные переменные
+        if (! empty($opts['cleanText']) && preg_match($regex, $string)) {
+            return '';
+        }
+
+        // удаление ненайденных переменных
+        if (! empty($opts['cleanVars'])) {
+            $string = (string)preg_replace($regex, '', $string);
+        }
+
+        return $string;
     }
 }
