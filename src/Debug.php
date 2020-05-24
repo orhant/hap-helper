3f2455304a7455a14c0f0f7a06fea611f42622e0
Igor A Tarasov <develop@dicr.org>
2020-05-24 13:54:55 +0500

    Debug

--- src/Debug.php
+++ src/Debug.php
@@ -3,13 +3,16 @@
  * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 15.01.20 12:42:19
+ * @version 24.05.20 13:54:39
  */
 
 declare(strict_types = 1);
 
 namespace dicr\helper;
 
+use function count;
+use function reset;
+use function var_dump;
 use const YII_DEBUG;
 
 /**
@@ -20,22 +23,15 @@ class Debug
     /**
      * Дамп значения в html.
      *
-     * @param mixed $val
-     * @param bool $exit выйти после дампа
+     * @param array $vars
      * @noinspection PhpUnused
      */
-    public static function xmp($val, bool $exit = true)
+    public static function xmp(...$vars)
     {
-        if (! YII_DEBUG) {
-            return;
-        }
-
-        echo '<xmp>';
-
-        /** @noinspection ForgottenDebugOutputInspection */
-        var_dump($val);
-
-        if ($exit) {
+        if (YII_DEBUG && ! empty($vars)) {
+            echo '<xmp>';
+            /** @noinspection ForgottenDebugOutputInspection */
+            var_dump(count($vars) > 1 ? $vars : reset($vars));
             exit;
         }
     }
